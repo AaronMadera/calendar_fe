e6f5858e9700baf8dd9444dc2c5093b6ff08860a
aamc <aaron@jmjcsoftware.com>
2020-07-05 23:09:54 -0500

    [ADD] Layout component, not ready yet.

---
+++ src/views/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Layout</h1>
+         <router-view :key="$route.fullPath"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Layout',
+
+}
+</script>
